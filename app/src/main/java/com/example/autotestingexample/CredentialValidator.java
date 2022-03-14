e8ac17871c5efa69c27bc69fabb38aadfbe4e239
CKLFH <christopher.koester@leibniz-fh.de>
2022-03-14 16:31:31 +0100

    Add mock

--- app/src/main/java/com/example/autotestingexample/CredentialValidator.java
+++ app/src/main/java/com/example/autotestingexample/CredentialValidator.java
@@ -23,14 +23,15 @@ public class CredentialValidator {
         if(password == null || password.isEmpty() || password.length() < 8){
             return false;
         }
-
-        Pattern pattern;
-        Matcher matcher;
-        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
-        pattern = Pattern.compile(PASSWORD_PATTERN);
-        matcher = pattern.matcher(password);
-
-        return matcher.matches();
+        return isPasswordStrong(password);
     }
 
+    public boolean isPasswordStrong(String password){
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
